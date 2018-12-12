f21c97e7b457868533454b72690ff44b949c38e5
CarlosCamacho <carcasi_5@hotmail.com>
2018-12-12 19:11:48 +0100

    ejercicios

---
+++ Assets/script/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D capsula;
+
+    public float velocidad = 10f;
+    Vector2 posicionObjetivo;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        posicionObjetivo = transform.position;
+        capsula = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movimientoTeclas()
+        Vector2 mousePosition = Input.mousePosition;
+    
+        
+    if (Input.GetMouseButton(0)){
+        posicionObjetivo = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
+
+        gameObject.transform.position = Vector2.MoveTowards(transform.position, new Vector2(posicionObjetivo.x, transform.position.y), velocidad * Time.deltaTime);
+        
+
+    }
+
+    void movimientoTeclas()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+        capsula.velocity = velocidad * new Vector2(horizontal, 0);
+    }
+    
+
+
+
+
+
+}
---
+++ ClaseJavi/Assets/script/pegarObjetos.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pegarObjetos : MonoBehaviour {
+
+    
+    public List<GameObject> arrayObjetos = new List<GameObject>();
+    GameObject ultimoObjeto;
+    public GameObject disparador;
+    public Text puntuacion;
+    int puntos = 0;
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+        puntuacion.text = "" + puntos;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        puntuacion.text = "" + puntos;
+        if (Input.GetKeyDown(KeyCode.Space)){
+
+            
+            Rigidbody2D ultimoObjeto = arrayObjetos[arrayObjetos.Count - 1].GetComponent<Rigidbody2D>() ;
+            GameObject ultimoObjetoFinalPlusUltra = arrayObjetos[arrayObjetos.Count - 1];
+            ultimoObjeto.transform.parent = null;
+            
+
+            ultimoObjeto.simulated = true;
+            ultimoObjeto.AddForce(new Vector2(450f, 450f));
+            arrayObjetos.Remove(ultimoObjetoFinalPlusUltra);
+            //transform.DetachChildren();
+            puntos--;
+
+        }
+        
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if(collision.gameObject.CompareTag("Objeto")){
+
+            Rigidbody2D Objeto;
+
+
+            collision.transform.position = disparador.transform.position;
+            Objeto = collision.gameObject.GetComponent<Rigidbody2D>();
+            Objeto.simulated = false;
+            collision.transform.parent = transform;
+            
+
+            GameObject nuevoObjeto = collision.gameObject;
+            arrayObjetos.Add(nuevoObjeto);
+            puntos++;
+            
+
+
+        }
+	}
+}
